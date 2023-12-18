57054a0ba1946b91825ff9be4d194edc243bf00a
Kartik Garg <gargkartik182@gmail.com>
2023-12-18 12:55:48 +0530

    changes

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ require('dotenv').config();
 const port =process.env.PORT||5000;
 const app = express();
 const{uploadFiles}=require("./controllers/uploadFiles.js");
+const cors=require('cors');
 
 const dB=require('./middlewares/dB.js')
 dB.connectTodB();
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine','ejs')
 const excelRoutes=require("./routes/excelRoutes.js");
