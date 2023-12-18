83b13c820f972f25f028aac3fd7fdf77d8d1e361
Kartik Garg <gargkartik182@gmail.com>
2023-12-18 15:08:29 +0530

    changes

--- routes/excelRoutes.js
+++ routes/excelRoutes.js
@@ -3,21 +3,21 @@ const router=express.Router();
 // const multer = require('multer');
 const fs=require('fs');
 // const upload = multer({ storage: multer.memoryStorage() });
-// const multer = require("multer");
+const multer = require("multer");
 
-// const storage=multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         const uploadDir = './uploads';
-//         if (!fs.existsSync(uploadDir)) {
-//             fs.mkdirSync(uploadDir);
-//           }
-//           return cb(null, uploadDir);
-//       },
-//       filename: function (req, file, cb) {
-//         return cb(null,`${Date.now()}-${file.originalname}`);
-//       }
-// })
-// const upload = multer({  storage});
+const storage=multer.diskStorage({
+    destination: function (req, file, cb) {
+        const uploadDir = './uploads';
+        if (!fs.existsSync(uploadDir)) {
+            fs.mkdirSync(uploadDir);
+          }
+          return cb(null, uploadDir);
+      },
+      filename: function (req, file, cb) {
+        return cb(null,`${Date.now()}-${file.originalname}`);
+      }
+})
+const upload = multer({  storage});
 const {certificateGen}=require("../controllers/certificateGen")
 const{uploadFiles}=require("../controllers/uploadFiles");
 
