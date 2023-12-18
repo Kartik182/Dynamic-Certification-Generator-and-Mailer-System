d03a57bd9fec06417ce44a6b938a9fc13503326f
Kartik182 <91744183+Kartik182@users.noreply.github.com>
2023-12-18 14:32:40 +0530

    Update excelRoutes.js

--- routes/excelRoutes.js
+++ routes/excelRoutes.js
@@ -2,7 +2,7 @@ const express=require('express');
 const router=express.Router();
 // const multer = require('multer');
 const fs=require('fs');
-const upload = multer({ storage: multer.memoryStorage() });
+// const upload = multer({ storage: multer.memoryStorage() });
 const multer = require("multer");
 
 const storage=multer.diskStorage({
